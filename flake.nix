c022846b2adfc35e91a6a0ea8978bfd18f75281e
Your Name <you@example.com>
2024-10-30 00:42:17 +0800

    add haskell

--- flake.nix
+++ flake.nix
@@ -1,13 +1,15 @@
 {
   description = "Development environment templates";
 
-  outputs = {
-    self,
-  }: {
+  outputs = {self}: {
     templates = {
       cpp = {
         path = ./templates/cpp;
         description = "C++ development environment with configurable compiler";
+      };
+      haskell = {
+        path = ./templates/haskell;
+        description = "haskell";
       };
 
       default = self.templates.cpp;
