912bfabe869cf533e795675ff27fe8a7177380bc
TomatoCream <dingfengwong@gmail.com>
2025-01-20 10:32:59 +0800

    add to zig

--- flake.nix
+++ flake.nix
@@ -1,22 +1,32 @@
 {
   description = "Development environment templates";
 
-  outputs = {self}: {
-    templates = {
-      cpp = {
-        path = ./templates/cpp;
-        description = "C++ development environment with configurable compiler";
-      };
-      haskell = {
-        path = ./templates/haskell;
-        description = "haskell";
-      };
-      python = {
-        path = ./templates/python;
-        description = "python";
-      };
+  outputs =
+    { self }:
+    {
+      templates = {
+        cpp = {
+          path = ./templates/cpp;
+          description = "C++ development environment with configurable compiler";
+        };
+        rust = {
+          path = ./templates/rust;
+          description = "rust";
+        };
+        zig = {
+          path = ./templates/zig;
+          description = "zig";
+        };
+        haskell = {
+          path = ./templates/haskell;
+          description = "haskell";
+        };
+        python = {
+          path = ./templates/python;
+          description = "python";
+        };
 
-      default = self.templates.cpp;
+        default = self.templates.cpp;
+      };
     };
-  };
 }
